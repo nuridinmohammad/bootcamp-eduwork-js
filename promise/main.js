5444529c2d12334a3aa60cb44dc3c9a7d41c3c5e
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2023-04-16 07:48:13 +0700

    promise update

--- promise/main.js
+++ promise/main.js
@@ -1,3 +1,5 @@
+// import image from './img/gambar.jpeg'
+
 const searchInput = document.getElementById("input-search");
 const news = document.getElementById("news");
 
@@ -6,10 +8,9 @@ searchInput.addEventListener("keyup", findMovie);
 function findMovie(e) {
   const searchValue = e.target.value.trim();
   if (searchValue.length > 0) {
-    console.log(searchValue);
     getNews(searchValue);
   } else {
-    return headLineNews()
+    return headLineNews();
   }
 }
 
@@ -19,7 +20,7 @@ async function getNews(value) {
   const url =
     "https://newsapi.org/v2/everything?" +
     `q=${value}&` +
-    "apiKey=defeb60311944f86b75cd4a0700a4957";
+    "apiKey=05163d63e0d14a79a11f4b30a8eecf29";
   const req = new Request(url);
 
   try {
@@ -38,33 +39,40 @@ async function displayNews(data) {
     newsItem.dataset.id = await data[i].source.id;
     newsItem.classList.add(".news-item");
     newsItem.innerHTML = `
-    <ul>
-        <li>${await data[i].title}</li>
-    </ul>
+    <div class="news-item">
+      <div class="header">
+        <h3>${data[i].title}</h3>
+          </div>
+            <div class="body">
+              <div class="image">
+              <img src=${data[i].urlToImage } alt="image-news" srcset="">
+            </div>
+          <div class="link"><a href=${data[i].url} target="_blank">More..</a></div>
+      </div>
+    </div>
     `;
     news.appendChild(newsItem);
   }
 }
 
-async function displayError(error) {
-  alert(await error.message);
-}
-
 window.onload = async function () {
-  await headLineNews()
+  await headLineNews();
 };
 
 async function headLineNews() {
   const url =
     "https://newsapi.org/v2/top-headlines?" +
     "country=id&" +
-    "apiKey=defeb60311944f86b75cd4a0700a4957";
+    "apiKey=05163d63e0d14a79a11f4b30a8eecf29";
   const req = new Request(url);
 
   try {
     const res = await fetch(req);
     const data = await res.json();
-    if (data.status !== "error") await displayNews(data.articles);
+    console.log(data);
+    setTimeout(() => {
+      if (data.status !== "error") displayNews(data.articles);
+    }, 3000);
   } catch (error) {
     if (data.status === "error") await displayError(error);
   }
